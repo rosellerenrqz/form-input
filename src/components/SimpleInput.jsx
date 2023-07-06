75b3d3fd9d2f953fb695648812a1da8b5b5a0781
Vincent Enriquez <enriquezroseller23@gmail.com>
2023-07-06 17:40:01 +0800

    adding additional email input with validation. Code needs to be slimmer and readable since it has redundancy state validity.

--- src/components/SimpleInput.jsx
+++ src/components/SimpleInput.jsx
@@ -4,12 +4,20 @@ const SimpleInput = () => {
   const [enteredName, setEnteredName] = useState("");
   const [enteredNameTouched, setEnteredNameTouched] = useState(false);
 
+  const [enteredEmail, setEnteredEmail] = useState("");
+  const [enteredEmailTouched, setEnteredEmailTouched] = useState(false);
+
   const enteredNameIsValid = enteredName.trim() !== "";
+
+  const entereEmailIsValid =
+    enteredEmail.trim() !== "" && enteredEmail.includes("@");
+
   const nameInputInvalid = !enteredNameIsValid && enteredNameTouched;
+  const emailInputInvalid = !enteredEmail && enteredEmailTouched;
 
   let formIsValid = false;
 
-  if (enteredNameIsValid) {
+  if (enteredNameIsValid && entereEmailIsValid) {
     formIsValid = true;
   } else {
     formIsValid = false;
@@ -19,26 +27,42 @@ const SimpleInput = () => {
     setEnteredName(e.target.value);
   };
 
+  const emailInputHandler = (e) => {
+    setEnteredEmail(e.target.value);
+  };
+
   const nameInputBlurHandler = () => {
     setEnteredNameTouched(true);
+  };
+
+  const emailInputBlurHandler = () => {
+    setEnteredEmailTouched(true);
   };
 
   const submitHandler = (e) => {
     e.preventDefault();
 
     setEnteredNameTouched(true);
-    if (!enteredNameIsValid) {
+
+    if (!enteredNameIsValid && !entereEmailIsValid) {
       return;
     }
 
     setEnteredName("");
     setEnteredNameTouched(false); //adding this so after the submit and input will be empty, it wont show an error.
+
+    setEnteredEmail("");
+    setEnteredEmailTouched(false); //adding this so after the submit and input will be empty, it wont show an error.
+
     console.log(enteredName);
+    console.log(enteredEmail);
   };
 
-  const inputInvalidHandler = nameInputInvalid
-    ? "border-2 border-red-600 bg-red-200 focus:border-2 focus:border-red-500 focus:bg-red-100"
-    : "";
+  const invalidStyle =
+    "border-2 border-red-600 bg-red-200 focus:border-2 focus:border-red-500 focus:bg-red-100";
+
+  const inputInvalidHandler = nameInputInvalid ? invalidStyle : "";
+  const emailInvalidHandler = emailInputInvalid ? invalidStyle : "";
 
   return (
     <>
@@ -55,8 +79,23 @@ const SimpleInput = () => {
             onChange={nameInputHandler}
           />
           {nameInputInvalid && (
-            <p className="text-red-500 font-bold text-sm">
+            <p className="text-red-500 font-bold text-sm mb-4">
               Name must not be empty.
+            </p>
+          )}
+          <label htmlFor="email">E-mail Address</label>
+          <input
+            type="text"
+            id="email"
+            placeholder="E-mail Address"
+            value={enteredEmail}
+            className={emailInvalidHandler}
+            onBlur={emailInputBlurHandler}
+            onChange={emailInputHandler}
+          />
+          {emailInputInvalid && (
+            <p className="text-red-500 font-bold text-sm mb-4">
+              Enter a valid E-mail.
             </p>
           )}
         </div>
