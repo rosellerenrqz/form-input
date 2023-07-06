89800fbf1ec7ee58ed1cbb2d4cfc4ee1df77b50e
Vincent Enriquez <enriquezroseller23@gmail.com>
2023-07-06 13:48:30 +0800

    fixing issue on enteredname valid from true to false. since it is not really true at first. refactoring the validations.

--- src/components/SimpleInput.jsx
+++ src/components/SimpleInput.jsx
@@ -1,8 +1,15 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 
 const SimpleInput = () => {
   const [enteredName, setEnteredName] = useState("");
-  const [enteredNameIsValid, setEnterNameIsValid] = useState(true);
+  const [enteredNameIsValid, setEnterNameIsValid] = useState(false);
+  const [enteredNameTouched, setEnteredNameTouched] = useState(false);
+
+  useEffect(() => {
+    if (enteredNameIsValid) {
+      console.log("valid");
+    }
+  }, [setEnterNameIsValid]);
 
   const nameInputHandler = (e) => {
     setEnteredName(e.target.value);
@@ -10,6 +17,8 @@ const SimpleInput = () => {
 
   const submitHandler = (e) => {
     e.preventDefault();
+
+    setEnteredNameTouched(true);
 
     if (enteredName.trim() === "" || enteredName.length <= 0) {
       setEnterNameIsValid(false);
@@ -22,9 +31,11 @@ const SimpleInput = () => {
     console.log(enteredName);
   };
 
-  const inputInvalidHandler = enteredNameIsValid
-    ? ""
-    : "border-2 border-red-600 bg-red-200 focus:border-2 focus:border-orange-500 focus:bg-orange-200";
+  const nameInputInvalid = !enteredNameIsValid && enteredNameTouched;
+
+  const inputInvalidHandler = nameInputInvalid
+    ? "border-2 border-red-600 bg-red-200 focus:border-2 focus:border-red-500 focus:bg-red-100"
+    : "";
 
   return (
     <>
@@ -35,10 +46,11 @@ const SimpleInput = () => {
             type="text"
             id="name"
             placeholder="Name"
+            value={enteredName}
             className={inputInvalidHandler}
             onChange={nameInputHandler}
           />
-          {!enteredNameIsValid && (
+          {nameInputInvalid && (
             <p className="text-red-500 font-bold text-sm">
               Name must not be empty.
             </p>
